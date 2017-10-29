c708124091e87844ff34270f2a4217f64ddbd83c
djo <declan.l.johnson@gmail.com>
2017-10-29 15:19:51 -0500

    Add ICO variables

--- src/ICO.sol
+++ src/ICO.sol
@@ -1,6 +1,22 @@
 pragma solidity ^0.4.2;
 
 contract ICO {
+	
+	address public beneficiary;
+	uint public ethereumRaised;
+	uint public ethereumMax;
+	uint public deadline;
+	uint public currentPrice;
+	token public cortexToken;
+	boss public controllingBoard;
+	bool public goalReached;
+	bool public allowRefunds;
+	uint public ethereumReleased;
 
+	event Purchase(address indexed _buyer, uint _ethereumSpent, uint _cortexReceived);
+	event SaleOpen(uint _cortexAvailable);
+	event SaleClosed(uint _cortexUnsold);
+	event Withdrawal(address indexed _target, uint _ethereumAmount);
+	event Refund(address indexed _target, uint _cortexAmount);
 
 }
