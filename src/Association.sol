56c043014c6c6b504013ecd1c549cb226b389a4b
djo <declan.l.johnson@gmail.com>
2017-10-29 15:35:00 -0500

    Add variables, start events

--- src/Association.sol
+++ src/Association.sol
@@ -1,5 +1,19 @@
 pragma solidity ^0.4.2;
 
 contract Association {
+	
+	uint public minimumQuorum;
+	uint[] public scheduledVotes;
+	uint[] public pastResults;
+	uint public voteLength;
+	token public cortexToken;
+	ico public controlledContract;
+
+	event VoteCast();
+	event VoteStart();
+	event VoteEnd();
+	event Results();
+
+	
 
 }
