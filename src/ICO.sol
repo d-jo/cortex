304b79d3cb95bebdb8abfabd439aef31a25714cd
djo <declan.l.johnson@gmail.com>
2017-10-30 13:22:40 -0500

    Start on methods, constructor, ect

--- src/ICO.sol
+++ src/ICO.sol
@@ -1,17 +1,27 @@
 pragma solidity ^0.4.2;
 
+import "./Cortex.sol";
+import "./Association.sol";
+
 contract ICO {
 	
+	struct Vote {
+		address voter;
+		uint vote;
+	}
+
 	address public beneficiary;
 	uint public ethereumRaised;
 	uint public ethereumMax;
+	uint public saleStartTime;
 	uint public deadline;
 	uint public currentPrice;
-	token public cortexToken;
-	boss public controllingBoard;
+	Cortex public cortexToken;
+	Association public controllingBoard;
 	bool public goalReached;
 	bool public allowRefunds;
 	uint public ethereumReleased;
+	Vote[] public votes;
 
 	event Purchase(address indexed _buyer, uint _ethereumSpent, uint _cortexReceived);
 	event SaleOpen(uint _cortexAvailable);
@@ -19,4 +29,26 @@ contract ICO {
 	event Withdrawal(address indexed _target, uint _ethereumAmount);
 	event Refund(address indexed _target, uint _cortexAmount);
 
+	modifier coinholders {
+		require(token.balanceOf(msg.sender) > 0);
+		_;
+	}
+
+	modifier whenRefundable {
+		require(allowRefund);
+		_;
+	}
+
+	function ICO() public {
+		ethereumRaised = 0 ether;
+		ethereumMax = 3000 ether;
+		saleStartTime = now + 7 days;
+		deadline = saleStartTime + 7 days;
+		currentPrice = 12;
+		cortexToken = new Cortex();
+		controllingBoard = new Association();
+		goalReached = false;
+		allowRefunds = false;
+		ethereumReleased = 0 ether;
+	}
 }
