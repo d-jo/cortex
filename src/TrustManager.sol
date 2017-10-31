45a2b92cba2b5f23d4c9d1896040de42f1aeb7c9
djo <declan.l.johnson@gmail.com>
2017-10-31 13:43:33 -0500

    Create TrustManager.sol

---
+++ src/TrustManager.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.2;
+
+contract TrustManager {
+
+	mapping (address => bool) private trusted;
+	event UserTrusted(address indexed truster, address indexed trustee);
+	event UserUntrusted(address indexed trusted, address indexed target);
+
+	function TrustManager() public {
+		trusted[msg.sender] = true;
+	}
+
+	modifier onlyTrusted {
+		require(trusted[msg.sender]);
+		_;
+	}
+
+	function addTrusted(address target) onlyTrusted public {
+		trusted[target] = true;
+		UserTrusted(msg.sender, target);
+	}
+
+	function removeTrusted(address target) onlyTrusted public {
+		trusted[target] = false;
+		UserUntrusted(msg.sender, target);
+		
+	}
+
+}
