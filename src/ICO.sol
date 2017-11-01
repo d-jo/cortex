413ad5586ee8aa5103986f0d15dc9187bc985396
djo <declan.l.johnson@gmail.com>
2017-11-01 11:33:33 -0500

    Add goal-related code

--- src/ICO.sol
+++ src/ICO.sol
@@ -10,18 +10,20 @@ contract ICO is TrustManager {
 	uint public ethereumMax;
 	uint public saleStartTime;
 	uint public deadline;
-	uint public constant cortexPerEth = 10;
+	uint public constant cortexPerEther = 10;
 	Cortex public cortexToken;
 	Association public controllingBoard;
 	bool public goalReached;
 	uint public ethereumReleased;
+	uint public constant goalInEthereum = 25000 ether;
 	mapping (address => uint) contributions;
 
 	event Purchase(address indexed _buyer, uint _ethereumSpent);
 	event SaleOpen(uint _cortexAvailable);
 	event SaleClosed(uint _cortexUnsold);
+	event GoalReached();
 	event Withdraw(address indexed _target, uint _ethereumAmount);
-	event Refund(address indexed _target, uint _cortexAmount);
+	event Refund(address indexed _target, uint _ethereumAmount);
 
 	modifier duringSale {
 		require(now > saleStartTime);
@@ -62,18 +64,23 @@ contract ICO is TrustManager {
 		ethereumRaised += amt;
 		contributions[msg.sender] = amt;
 		Purchase(msg.sender, amt);
+		if (ethereumRaised >= goalInEthereum) {
+			goalReached = true;
+			GoalReached();
+		}
 		return (contributions[msg.sender] == amt);
 	}
 
 	function withdrawCortex(uint valueInCortex) public afterSuccesfulSale returns (bool) {
 		uint contrib = contributions[msg.sender];
+		uint cortexReward = (contributions[msg.sender] * 1 ether) * cortexPerEther;
+		return cortexToken.transfer(msg.sender, cortexReward);
 	}
 	
 	function withdrawEther(uint valueInEther) public onlyTrusted returns (bool) {
 		amount = valueInEther * 1 ether;
 		require(amount <= ethereumReleased);
 		ethereumReleased -= amount;
-		Withdraw(msg.sender, amount);
 		return msg.sender.transfer(amount);
 	}
 
@@ -82,7 +89,12 @@ contract ICO is TrustManager {
 		uint balance = contributions[msg.sender];
 		require(amt <= balance); 
 		contributions[msg.sender] = balance - amt; 
+		Refund(msg.sender, amount);
 		return msg.sender.transfer(amt);
+	}
+
+	function release(uint ethAmount) public onlyTrusted returns (bool) {
+		
 	}
 
 
