336db041ac098f4c098882084c6d208d885d1241
djo <declan.l.johnson@gmail.com>
2017-11-04 11:52:48 -0500

    Add ICO tests

--- src/ICO.t.sol
+++ src/ICO.t.sol
@@ -5,23 +5,54 @@ import "./ICO.sol";
 
 
 contract EasyTestICO is ICO {
-    uint public fakeTime;
+    uint public fakeTime = now;
 
     
     function getTime() internal returns (uint) {
         return fakeTime;
     }
 
+    function setTime(uint time) public {
+        fakeTime = time;
+    }
 
+    function checkIsSale() public returns (bool) {
+        return (getTime() > saleStartTime) && (getTime() < deadline);
+    }
 
 }
 
 contract ICOTest is DSTest {
 
-    ICO ico;
+    EasyTestICO ico;
 
     function setUp() public {
-        
+        ico = new EasyTestICO();
+        ico.setTime(now);
     }
+
+    function testFailOwnership() public {
+        assert(ico.controllingBoard().isTrusted(this));
+    }
+
+    function testFailBuyBeforeSale() public {
+        assert(ico.buy.value(1)(100));
+    }
+
+    function testIsSaleStarted() public {
+        ico.setTime(now + 8 days);
+        assert(ico.checkIsSale());
+    }
+
+    function testBuyWhenSale() public {
+        testIsSaleStarted();
+        assert(ico.buy.value(1 ether)(100 ether));
+    }
+
+    function testFailWithdrawDuringSale() public {
+        testBuyWhenSale();
+        assert(ico.withdrawRefund(1 ether));
+    }
+
 
 }
