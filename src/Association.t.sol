a0e172b2b7aabc52d4b2580dd3210ff8740af462
djo <declan.l.johnson@gmail.com>
2017-11-08 16:18:32 -0600

    Create full sale test for setup

--- src/Association.t.sol
+++ src/Association.t.sol
@@ -18,19 +18,69 @@ pragma solidity ^0.4.2;
 import "ds-test/test.sol";
 import "./ICO.sol";
 import "./Association.sol";
+import "./Cortex.sol";
 import "./ICO.t.sol";
+
+
+contract Holder {
+    
+    EasyTestICO ico;
+
+    function Holder(EasyTestICO icoToBuy) {
+        ico = icoToBuy;
+    }
+
+    function buy() returns (bool){
+        return ico.buy.value(11000 ether)(100000 ether);
+    }
+
+    function withdraw() returns (bool) {
+        return ico.withdrawCortex();
+    }
+
+    function () payable {
+
+    }
+}
 
 
 contract AssociationTest is DSTest {
 
     EasyTestICO ico;
     Association board;
+    Cortex token;
+    Holder mac;
+    Holder dennis;
+    Holder charlie;
 
     function setUp() public {
         ico = new EasyTestICO();
         board = ico.controllingBoard();
+        token = ico.cortexToken();
+        mac = new Holder(ico);
+        dennis = new Holder(ico);
+        charlie = new Holder(ico);
+        mac.send(15000 ether);
+        dennis.send(15000 ether);
+        charlie.send(15000 ether);
     }
 
+    function testSuccessfulSale() public {
+        ico.setTime(now + 8 days);
+        assert(mac.buy());
+        assert(dennis.buy());
+        assert(charlie.buy());
+        ico.setTime(ico.fakeTime() + 8 days);
+        assert(mac.withdraw());
+        assert(dennis.withdraw());
+        assert(charlie.withdraw());
+    }
+
+    function testNewProposal() public {
+        
+    }
+
+    
 
     
 }
